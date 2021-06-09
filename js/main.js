a0006cdaa151cf24556a8376cd2332f4721508ca
Yahya Shannat <yahyashannat@gmail.com>
2021-06-09 13:45:23 +0300

    bootstrap website

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+    //Adjust Slider Height
+    var winH = $(window).innerHeight();
+        upperH = $('.upper-bar').innerHeight();
+        navH = $('.navbar').innerHeight();
+    
+    $('.slider, .carousel-item').height(winH - (upperH + navH));
+    //Featured Work Shuffle
+    $('.featured-work ul li').on('click', function(){
+        $(this).addClass('active').siblings().removeClass('active');
+        if ($(this).data('class') === 'all') {
+            $('.shuffle-imgs .col-lg-3').css('opacity', '1')
+        } else {
+            $('.shuffle-imgs .col-lg-3').css('opacity', '.09')
+            $($(this).data('class')).parent().css('opacity', '1')
+        }
+    });
+});
+
+
